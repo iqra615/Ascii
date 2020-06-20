5c964c5f59e17c4dfd6604ab84e3d58144e61f50
HP 6th <iqra615>
2020-06-20 08:08:47 -0700

    Ascii

---
+++ src/come/codewithiqra/Main.java
@@ -0,0 +1,9 @@
+package come.codewithiqra;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int chr = 'A';
+        System.out.println("The Ascii value of A is:" + chr);
+    }
+}
